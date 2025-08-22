5cdf78814ffc8e492d0cccee4d57391a7302951f
F4NTOM-007 <yashvermacs007@gmail.com>
2025-08-22 16:23:07 +0530

    BackEnd

--- Day 08/Thought Website/thoughtweb/signup_api/urls.py
+++ Day 08/Thought Website/thoughtweb/signup_api/urls.py
@@ -1,8 +1,19 @@
-from django.urls import path
+"""from django.urls import path
 from .views import UserSignupView, UserSignupDetailView, UserLoginView
 
 urlpatterns = [
     path('signup/', UserSignupView.as_view(), name='user_signup'),
     path('signup/<int:pk>/', UserSignupDetailView.as_view(), name='user_signup_detail'),
     path('login/', UserLoginView.as_view(), name='user_login'),
+]
+
+"""
+
+from django.urls import path
+from .views import signup, login_view, logout_view
+
+urlpatterns = [
+    path('signup/', signup, name='signup'),
+    path('login/', login_view, name='login'),
+    path('logout/', logout_view, name='logout'),
 ]
